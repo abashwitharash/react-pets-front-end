0eeb3db219e4865214d7cc6390ff6f96e2b296f6
Arash <arashsaleh798@gmail.com>
2025-03-12 12:15:53 -0700

    UI experence can see dogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react'
 import * as petService from './services/petService';
 import PetList from './components/PetList/PetList';
+import PetDetail from './components/PetDetail/PetDetail';
+import PetForm from './components/PetForm/PetForm';
 import './App.css'
 
 const App = () => {
   const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   
   useEffect(() => {
@@ -24,10 +28,50 @@ const App = () => {
     fetchPets();
   }, []);
 
+  const handleSelect = (pet) => {
+    setSelected(pet)
+  }
+
+  const handleFormView = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddPet = async (formData) => {
+    try {
+      // Call petService.create, assign return value to newPet
+      const newPet = await petService.create(formData);
+
+      if (newPet.err) {
+        throw new Error(newPet.err);
+      }
+  
+
+      setPets([newPet, ...pets]);
+       // Close the form after we've added the new pet
+    setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+
   return (
     <>
-      <PetList pets={pets} />
+      <PetList
+        pets={pets}
+        handleSelect={handleSelect}
+        handleFormView={handleFormView}
+        isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+        <PetForm handleAddPet={handleAddPet} />
+      ) : (
+        <PetDetail selected={selected} />
+      )}
     </>
   );
 };
+
+
 export default App;
---
+++ src/components/PetForm/PetForm.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const PetForm = (props) => {
+  // formData state to control the form.
+  const [formData, setFormData] = useState({
+    name: '',
+    age: '',
+    breed: '',
+  });
+
+  // handleChange function to update formData state.
+  const handleChange = (evt) => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    props.handleAddPet(formData);
+    // Right now, if you add a pet and submit the form,
+    // the data entered will stay on the page. We'll fix this soon.
+  };
+
+  // And finally, the form itself.
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name"> Name </label>
+        <input
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="age"> Age </label>
+        <input
+          id="age"
+          name="age"
+          value={formData.age}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="breed"> Breed </label>
+        <input
+          id="breed"
+          name="breed"
+          value={formData.breed}
+          onChange={handleChange}
+        />
+        <button type="submit">Add New Pet</button>
+      </form>
+    </div>
+  );
+};
+
+export default PetForm;

4cf5e9fdda0e76e920d8e842a6dec0159fd63f7d
Arash <arashsaleh798@gmail.com>
2025-03-13 13:39:28 -0700

    delete function created

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,8 @@ const App = () => {
     setSelected(pet)
   }
 
-  const handleFormView = () => {
+  const handleFormView = (pet) => {
+    if (!pet._id) setSelected(null);
     setIsFormOpen(!isFormOpen);
   };
 
@@ -54,6 +55,47 @@ const App = () => {
     }
   };
 
+  const handleUpdatePet = async (formData, petId) => {
+    try {
+      const updatedPet = await petService.update(formData, petId);
+  
+      // handle potential errors
+      if (updatedPet.err) {
+        throw new Error(updatedPet.err);
+      }
+  
+      const updatedPetList = pets.map((pet) => (
+        // If the _id of the current pet is not the same as the updated pet's _id,
+        // return the existing pet.
+        // If the _id's match, instead return the updated pet.
+        pet._id !== updatedPet._id ? pet : updatedPet
+      ));
+      // Set pets state to this updated array
+      setPets(updatedPetList);
+      // If we don't set selected to the updated pet object, the details page will
+      // reference outdated data until the page reloads.
+      setSelected(updatedPet);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeletePet = async (petId) => {
+    try {
+      const deletedPet = await petService.deletePet(petId);
+
+      if (deletedPet.err) {
+        throw new Error(deletedPet.err);
+      }
+
+      setPets(pets.filter((pet) => pet._id !== deletedPet._id));
+      setSelected(null);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
 
   return (
@@ -64,10 +106,19 @@ const App = () => {
         handleFormView={handleFormView}
         isFormOpen={isFormOpen}
       />
+      {/* Pass handleUpdatePet to PetForm */}
       {isFormOpen ? (
-        <PetForm handleAddPet={handleAddPet} />
+        <PetForm
+          handleAddPet={handleAddPet}
+          selected={selected}
+          handleUpdatePet={handleUpdatePet}
+        />
       ) : (
-        <PetDetail selected={selected} />
+        <PetDetail 
+        selected={selected} 
+        handleFormView={handleFormView}
+        handleDeletePet={handleDeletePet}
+        />
       )}
     </>
   );
--- src/components/PetForm/PetForm.jsx
+++ src/components/PetForm/PetForm.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 
 const PetForm = (props) => {
-  // formData state to control the form.
-  const [formData, setFormData] = useState({
+const initialState = {
     name: '',
     age: '',
     breed: '',
-  });
+}
+
+
+  // If pet data has been passed as props, we set formData as that pet object.
+  // Otherwise, we can assume this is a new pet form and use the empty
+  // initialState object.
+  const [formData, setFormData] = useState(
+    props.selected ? props.selected : initialState
+  )
 
   // handleChange function to update formData state.
   const handleChange = (evt) => {
@@ -15,9 +22,12 @@ const PetForm = (props) => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    props.handleAddPet(formData);
-    // Right now, if you add a pet and submit the form,
-    // the data entered will stay on the page. We'll fix this soon.
+    if (props.selected) {
+      // Don't forget to pass both formData and the ._id!
+      props.handleUpdatePet(formData, props.selected._id);
+    } else {
+      props.handleAddPet(formData);
+    }
   };
 
   // And finally, the form itself.
@@ -47,7 +57,9 @@ const PetForm = (props) => {
           value={formData.breed}
           onChange={handleChange}
         />
-        <button type="submit">Add New Pet</button>
+        <button type="submit">
+          {props.selected ? 'Update Pet' : 'Add New Pet'}
+            </button>
       </form>
     </div>
   );
